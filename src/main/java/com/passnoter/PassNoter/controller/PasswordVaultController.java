17f378051173df7d14e84e89327cca527ba06619
Sonu <animenarusuke@gmail.com>
2026-03-10 15:20:51 +0530

    updated backend configuration

--- src/main/java/com/passnoter/PassNoter/controller/PasswordVaultController.java
+++ src/main/java/com/passnoter/PassNoter/controller/PasswordVaultController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/vault")
 public class PasswordVaultController {
     @Autowired
--- src/main/java/com/passnoter/PassNoter/controller/UserAuthController.java
+++ src/main/java/com/passnoter/PassNoter/controller/UserAuthController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserAuthController {
     @Autowired
     UserAuthRepository userAuthRepository;
